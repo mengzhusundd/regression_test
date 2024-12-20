8d5d5e20a2db3c1d591541651823759d5775b5b8
mengzhusundd <zoey.sun@doordash.com>
2024-12-19 23:25:17 -0800

    add 15 deliveries to golden dataset

--- convert.py
+++ convert.py
@@ -21,27 +21,29 @@ for raw_result in data_frame['PREDICTION_RESULT']:
     for item in json_result:
         value = item['value']
         if item['key'] == 'delivery_uuid':
-            result_str[0] = "delivery_uuid is: {} \n".format(value)
+            result_str[0] = "\"delivery_uuid\": {},".format(value)
         # total amount is the same thing as grand total 
         if item['key'] == 'total_amount':
-            result_str[1] = "grand_total is: {} \n".format(value)
+            result_str[1] = "\"grand_total\": {},".format(value)
         if item['key'] == 'subtotal_amount':
-            result_str[2] = "subtotal_amount is: {}".format(value)
+            result_str[2] = "\"subtotal\": {},".format(value)
         if item['key'] == 'tax_amount':
-            result_str[3] = "tax_amount: {}".format(value)
+            result_str[3] = "\"tax_amount\": {},".format(value)
         if item['key'] == 'matched_item_info':
-            result_str[4] = "matched_item is: {}".format(value)
+            result_str[4] = "\"matched_item_info\": {},".format(value)
         if item['key'] == 'unmatched_item_info':
-            result_str[5] = "unmatched_item_info: {}".format(value)
+            result_str[5] = "\"unmatched_item_info\": {},".format(value)
         if item['key'] == 'bag_fee_info':
-            result_str[6] = "bag_fee: {}".format(value)
+            result_str[6] = "\"bag_fee\": {},".format(value)
         if item['key'] == 'bottle_fee_info':
-            result_str[7] = "bottle_fee: {}".format(value)
+            result_str[7] = "\"bottle_fee\": {},".format(value)
         if item['key'] == 'confidence_score':
-            result_str[8] = "confidence_score: {}".format(value)
+            result_str[8] = "\"confidence_score\": {},".format(value)
         if item['key'] == 'is_legible':
-            result_str[9] = "is_legible: {}".format(value)
+            result_str[9] = "\"is_legible\": {}".format(value)
+    print("{")
     for elm in result_str:
         print(elm)
         print()
+    print("}")
     print("============ NEXT DELIVERY =============")
