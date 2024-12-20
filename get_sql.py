ed64464c36d6c53abdb57f6927146f81affabf3f
mengzhusundd <zoey.sun@doordash.com>
2024-12-20 00:56:40 -0800

    add a function to get_sql

--- get_sql.py
+++ get_sql.py
@@ -1,24 +1,39 @@
 import json
+import os
 
 
-# getting a list of all delivery uuids to query results in MODE 
+# getting a list of all delivery uuids from Ani's prepared golden dataset to query results in MODE 
 deliveries = []
 
-input_file_path = "golden_dataset_ani_version.json"
+def getDeliveriesFromJson(): 
+  input_file_path = "golden_dataset_ani_version.json"
 
-with open(input_file_path, "r") as file:
-    # Parse the JSON string into a Python dictionary
-    data = json.load(file)
+  with open(input_file_path, "r") as file:
+      # Parse the JSON string into a Python dictionary
+      data = json.load(file)
 
-    # Access the 'golden_dataset' array
-    golden_dataset = data["golden_dataset"][0]
+      # Access the 'golden_dataset' array
+      golden_dataset = data["golden_dataset"][0]
 
-    # Extract all deliveries from the 'golden_dataset' array
-    for deliveryUuid in golden_dataset:
-        deliveries.append(deliveryUuid)
+      # Extract all deliveries from the 'golden_dataset' array
+      for deliveryUuid in golden_dataset:
+          deliveries.append(deliveryUuid)
 
-    print("Extracted deliveryUUIDs:")
-    print(deliveries)
+      print("Extracted deliveryUUIDs:")
+      print(deliveries)
+      return deliveries
+
+
+# get a list of delivery uuids from Argil dataset to query results in MODE
+def getDeliveriesFromDirectory():
+   argil_file_path = "/Users/zoey.sun/Projects/argil/models/argil-nv-receipt-ocr/data/edge_cases/audit_2024-06-10"
+   delivery_files = os.listdir(argil_file_path)
+   result = [file.split(".")[0] for file in delivery_files if file.endswith(".json")]
+   print(result)
+   return result
+deliveries = getDeliveriesFromDirectory()
+
+
 
 # Prepare the SQL in MODE
 deliveries_str = str(tuple(deliveries))
